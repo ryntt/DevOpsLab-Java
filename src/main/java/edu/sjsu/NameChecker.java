414d10f92576de93f9dbad254d599bbc8cef5eab
ryntt <76274559+ryntt@users.noreply.github.com>
2024-04-24 18:41:24 -0700

    Add files via upload

---
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -0,0 +1,32 @@
+package edu.sjsu;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NameChecker {
+    /**
+     * Checks if a given string is a valid name following these rules:
+     * - Between 2 to 40 characters
+     * - Contains only alphabetic characters, non-consecutive hyphens, or a single
+     * quote
+     * - Cannot start with a hyphen or single quote
+     * 
+     * @param input Name to check
+     * @return True if input is a valid name, else false
+     */
+    public static boolean check(String input) {
+        Pattern regex = Pattern.compile("^[a-zA-Z][a-zA-Z-']{1,39}$");
+        Matcher stringInput = regex.matcher(input);
+        if (stringInput.find()) {
+            String quotedPattern = "\".*\"";
+            String dashedPattern = "--";
+            if (input.matches(quotedPattern) || input.matches(dashedPattern)) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
